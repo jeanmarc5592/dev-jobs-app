fcb34338359875c0d55a13817fd0590b41eef5b2
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-05 10:56:03 +0100

    Add NotFoundView

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import JobsView from "../views/JobsView.vue";
 import JobDetailsView from "../views/JobDetailsView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,11 @@ const router = createRouter({
       path: "/jobs/:jobId",
       name: "job-details",
       component: JobDetailsView,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
