608815aa527e9213d0ed98507840f2df49dbd997
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-07 13:38:17 +0100

    WIP: Create and use PageLayout Component

---
+++ src/layouts/PageLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-container">
+    <TheHeader />
+    <main class="main-container">
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+import TheHeader from "../common/components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.main-container {
+  max-width: 1110px;
+  margin: 0 auto;
+}
+</style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,80 +1,15 @@
 <template>
-  <main>
-    <n-h1>H1</n-h1>
-    <n-h2>H2</n-h2>
-    <n-h3>H3</n-h3>
-    <n-h4>H4</n-h4>
-    <n-p>Body</n-p>
-    <n-button strong type="primary">Button 1</n-button>
-    <n-button strong type="info">Button 2</n-button>
-    <n-input type="text" placeholder="Filter by title, companies, expertise...">
-      <template #prefix>
-        <BaseSearchIcon />
-      </template>
-    </n-input>
-    <n-input type="text" placeholder="Filter by location...">
-      <template #prefix>
-        <BaseLocationIcon />
-      </template>
-    </n-input>
-    <n-space>
-      <BaseSunIcon />
-      <n-switch />
-      <BaseMoonIcon />
-    </n-space>
-    <n-checkbox> Full Time Only </n-checkbox>
-  </main>
+  <page-layout>
+    <p>Body</p>
+  </page-layout>
 </template>
 
 <script>
-import {
-  NP,
-  NH1,
-  NH2,
-  NH3,
-  NH4,
-  NButton,
-  NInput,
-  NSwitch,
-  NSpace,
-  NCheckbox,
-} from "naive-ui";
-import BaseSearchIcon from "../common/components/BaseSearchIcon.vue";
-import BaseLocationIcon from "../common/components/BaseLocationIcon.vue";
-import BaseSunIcon from "../common/components/BaseSunIcon.vue";
-import BaseMoonIcon from "../common/components/BaseMoonIcon.vue";
-import gqlRequest from "../graphql/request";
-import { JOBS_VIEW_QUERY } from "../graphql/queries";
+import PageLayout from "../layouts/PageLayout.vue";
 
 export default {
   components: {
-    NP,
-    NH1,
-    NH2,
-    NH3,
-    NH4,
-    NButton,
-    NInput,
-    NSwitch,
-    NSpace,
-    NCheckbox,
-    BaseSearchIcon,
-    BaseLocationIcon,
-    BaseSunIcon,
-    BaseMoonIcon,
-  },
-  async created() {
-    try {
-      const response = await gqlRequest({
-        query: JOBS_VIEW_QUERY,
-        variables: {
-          limits: 10,
-        },
-      });
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+    PageLayout,
   },
 };
 </script>
