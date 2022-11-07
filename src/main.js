c7ae33c335d4aab83097235da382b02b2f7cd484
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-07 13:38:05 +0100

    Use base.css instead of main.css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "./assets/main.css";
+import "./assets/base.css";
 
 const app = createApp(App);
 
