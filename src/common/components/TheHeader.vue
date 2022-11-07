26bd3f6006d474c8fea31f2c098b1e03ddab8dca
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-07 13:37:45 +0100

    WIP: Create Header UI

---
+++ src/common/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <BaseHeaderBackground />
+  </header>
+</template>
+
+<script>
+import BaseHeaderBackground from "./BaseHeaderBackground.vue";
+
+export default {
+  components: {
+    BaseHeaderBackground,
+  },
+};
+</script>
