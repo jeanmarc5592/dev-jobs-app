80eb5d2fbcb90732160c9fc7f1f1a12e2e37946e
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-07 18:55:11 +0100

    Add different headers for different screen sizes

--- src/common/components/TheHeader.vue
+++ src/common/components/TheHeader.vue
@@ -1,15 +1,26 @@
 <template>
-  <header>
-    <BaseHeaderBackground />
-  </header>
+  <header class="header-container"></header>
 </template>
 
-<script>
-import BaseHeaderBackground from "./BaseHeaderBackground.vue";
+<style lang="scss" scoped>
+.header-container {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 
-export default {
-  components: {
-    BaseHeaderBackground,
-  },
-};
-</script>
+  @media only screen and (min-width: 768px) {
+    background-image: url("../../assets/icons/bg-header-desktop.svg");
+    height: 160px;
+  }
+
+  @media only screen and (min-width: 375px) and (max-width: 768px) {
+    background-image: url("../../assets/icons/bg-header-tablet.svg");
+    height: 160px;
+  }
+
+  @media only screen and (max-width: 375px) {
+    background-image: url("../../assets/icons/bg-header-mobile.svg");
+    height: 136px;
+  }
+}
+</style>
