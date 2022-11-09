e14659ddbac60d832694a50a6e6a84ba0c9fb9b0
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-09 13:12:12 +0100

    Add "load more" functionality

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,8 +1,8 @@
 import { gql } from "graphql-request";
 
 export const JOBS_VIEW_QUERY = gql`
-  query getJobs($first: Int) {
-    jobs(first: $first) {
+  query getJobs($first: Int, $skip: Int) {
+    jobs(first: $first, skip: $skip) {
       company
       createdAt
       contract
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -8,24 +8,38 @@
         <JobCard v-bind="job" />
       </n-grid-item>
     </n-grid>
+    <n-space class="button-container" justify="center">
+      <n-button @click="loadMoreJobs" class="load-more-button" type="primary">
+        Load More</n-button
+      >
+    </n-space>
   </page-layout>
 </template>
 
 <script>
-import { NGrid, NGridItem } from "naive-ui";
+import { NGrid, NGridItem, NButton, NSpace } from "naive-ui";
 import PageLayout from "../layouts/PageLayout.vue";
 import TheJobFilters from "../common/components/TheJobFilters.vue";
 import gqlRequest from "../graphql/request";
 import { JOBS_VIEW_QUERY } from "../graphql/queries";
 import JobCard from "../modules/job/components/JobCard.vue";
 
+const ITEMS_PER_PAGE = 9;
+
 export default {
   components: {
     NGrid,
     NGridItem,
+    NButton,
+    NSpace,
     PageLayout,
     TheJobFilters,
     JobCard,
+  },
+  data() {
+    return {
+      page: 1,
+    };
   },
   computed: {
     jobsList() {
@@ -38,7 +52,7 @@ export default {
       const response = await gqlRequest({
         query: JOBS_VIEW_QUERY,
         variables: {
-          first: 9,
+          first: ITEMS_PER_PAGE,
         },
       });
       this.$store.commit("addJobs", response.jobs);
@@ -46,5 +60,28 @@ export default {
       console.error(error);
     }
   },
+  methods: {
+    async loadMoreJobs() {
+      try {
+        const response = await gqlRequest({
+          query: JOBS_VIEW_QUERY,
+          variables: {
+            first: ITEMS_PER_PAGE,
+            skip: this.page * ITEMS_PER_PAGE,
+          },
+        });
+        this.$store.commit("addJobs", response.jobs);
+        this.page += 2;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
+
+<style scoped lang="scss">
+.button-container {
+  margin-top: 56px;
+}
+</style>
