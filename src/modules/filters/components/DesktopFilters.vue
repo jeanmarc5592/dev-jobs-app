f3ba1b209d1190918f726835c59fd4d3eb0d34e9
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-13 12:29:34 +0100

    Refactor: Create and Use FiltersCheckbox Component

--- src/modules/filters/components/DesktopFilters.vue
+++ src/modules/filters/components/DesktopFilters.vue
@@ -7,50 +7,25 @@
       <TextInput inputType="location" />
     </n-grid-item>
     <n-grid-item class="button-container">
-      <n-checkbox :checked="fullTimeOnly" @update-checked="updateFullTimeOnly">
-        Full Time Only
-      </n-checkbox>
+      <FiltersCheckbox />
       <n-button attr-type="submit" type="primary"> Search </n-button>
     </n-grid-item>
   </FilterContainer>
 </template>
 
 <script>
-import { NCheckbox, NButton, NGridItem } from "naive-ui";
-import { mapState } from "vuex";
+import { NButton, NGridItem } from "naive-ui";
 import FilterContainer from "./FilterContainer.vue";
 import TextInput from "./FiltersTextInput.vue";
+import FiltersCheckbox from "./FiltersCheckbox.vue";
 
 export default {
   components: {
     NButton,
-    NCheckbox,
     NGridItem,
     TextInput,
     FilterContainer,
-  },
-  data() {
-    return {
-      page: 1,
-    };
-  },
-  computed: {
-    ...mapState({
-      search: (state) => state.jobs.filters.search,
-      location: (state) => state.jobs.filters.location,
-      fullTimeOnly: (state) => state.jobs.filters.fullTimeOnly,
-    }),
-  },
-  methods: {
-    updateSearch(value) {
-      this.$store.commit("updateFilters", { search: value });
-    },
-    updateLocation(value) {
-      this.$store.commit("updateFilters", { location: value });
-    },
-    updateFullTimeOnly(value) {
-      this.$store.commit("updateFilters", { fullTimeOnly: value });
-    },
+    FiltersCheckbox,
   },
 };
 </script>
---
+++ src/modules/filters/components/FiltersCheckbox.vue
@@ -0,0 +1,25 @@
+<template>
+  <n-checkbox :checked="fullTimeOnly" @update-checked="updateFullTimeOnly">
+    Full Time Only
+  </n-checkbox>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+import { NCheckbox } from "naive-ui";
+
+export default {
+  components: { NCheckbox },
+  computed: {
+    ...mapState({
+      fullTimeOnly: (state) => state.jobs.filters.fullTimeOnly,
+    }),
+  },
+  methods: {
+    ...mapMutations(["updateFilters"]),
+    updateFullTimeOnly(value) {
+      this.updateFilters({ fullTimeOnly: value });
+    },
+  },
+};
+</script>
