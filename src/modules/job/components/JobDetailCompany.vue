8927731905a5d7e8e17a49397c3baabedc18d8dd
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-16 14:13:43 +0100

    Refactor: Create and use JobDetailCompany Monolith

---
+++ src/modules/job/components/JobDetailCompany.vue
@@ -0,0 +1,88 @@
+<template>
+  <div :style="companyContainerInlineStyles" class="company-container">
+    <div :style="logoContainerInlineStyles" class="logo-container">
+      <img
+        class="logo"
+        :src="currentJobDetails && currentJobDetails.logo.url"
+      />
+    </div>
+    <div class="details-container">
+      <div>
+        <n-h2>{{ currentJobDetails && currentJobDetails.company }}</n-h2>
+        <n-p>{{ currentJobDetails && currentJobDetails.website }}</n-p>
+      </div>
+      <n-button type="info">Company Site</n-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { NH2, NP, NButton } from "naive-ui";
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    NH2,
+    NP,
+    NButton,
+  },
+  computed: {
+    ...mapGetters(["currentJobDetails", "currentThemeMode"]),
+    companyContainerInlineStyles() {
+      if (this.currentThemeMode === "light") {
+        return {
+          backgroundColor: "#fff",
+          transition: "all 0.3s",
+        };
+      }
+      return {
+        backgroundColor: "#19202D",
+        transition: "all 0.3s",
+      };
+    },
+    logoContainerInlineStyles() {
+      if (!this.currentJobDetails) return;
+      return {
+        backgroundColor: this.currentJobDetails.logoBackground,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.company-container {
+  display: flex;
+  border-radius: 6px;
+  .logo-container {
+    height: 140px;
+    width: 140px;
+    border-radius: 6px;
+    border-top-right-radius: 0px;
+    border-bottom-right-radius: 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .logo {
+      width: 60px;
+    }
+  }
+
+  .details-container {
+    width: 100%;
+    padding: 34px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h2 {
+      font-weight: 700;
+    }
+
+    p {
+      color: #6e8098;
+    }
+  }
+}
+</style>
--- src/views/JobDetailsView.vue
+++ src/views/JobDetailsView.vue
@@ -1,57 +1,23 @@
 <template>
   <page-layout :headerInnerStyles="headerInnerStyles">
     <template #header>
-      <div :style="companyContainerInlineStyles" class="company-container">
-        <div :style="logoContainerInlineStyles" class="logo-container">
-          <img
-            class="logo"
-            :src="currentJobDetails && currentJobDetails.logo.url"
-          />
-        </div>
-        <div class="details-container">
-          <div>
-            <n-h2>{{ currentJobDetails && currentJobDetails.company }}</n-h2>
-            <n-p>{{ currentJobDetails && currentJobDetails.website }}</n-p>
-          </div>
-          <n-button type="info">Company Site</n-button>
-        </div>
-      </div>
+      <JobDetailCompany />
     </template>
   </page-layout>
 </template>
 
 <script>
 import PageLayout from "../layouts/PageLayout.vue";
+import JobDetailCompany from "../modules/job/components/JobDetailCompany.vue";
 import { mapGetters, mapMutations, mapActions } from "vuex";
-import { NH2, NP, NButton } from "naive-ui";
 
 export default {
   components: {
-    NH2,
-    NP,
-    NButton,
     PageLayout,
+    JobDetailCompany,
   },
   computed: {
     ...mapGetters(["currentJobDetails", "currentThemeMode"]),
-    companyContainerInlineStyles() {
-      if (this.currentThemeMode === "light") {
-        return {
-          backgroundColor: "#fff",
-          transition: "all 0.3s",
-        };
-      }
-      return {
-        backgroundColor: "#19202D",
-        transition: "all 0.3s",
-      };
-    },
-    logoContainerInlineStyles() {
-      if (!this.currentJobDetails) return;
-      return {
-        backgroundColor: this.currentJobDetails.logoBackground,
-      };
-    },
     headerInnerStyles() {
       return {
         top: "100%",
@@ -86,39 +52,4 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-.company-container {
-  display: flex;
-  border-radius: 6px;
-  .logo-container {
-    height: 140px;
-    width: 140px;
-    border-radius: 6px;
-    border-top-right-radius: 0px;
-    border-bottom-right-radius: 0px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .logo {
-      width: 60px;
-    }
-  }
-
-  .details-container {
-    width: 100%;
-    padding: 34px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    h2 {
-      font-weight: 700;
-    }
-
-    p {
-      color: #6e8098;
-    }
-  }
-}
-</style>
+<style scoped lang="scss"></style>
