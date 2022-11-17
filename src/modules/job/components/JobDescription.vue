0f0edef18b0a0f600af79bfd855f0109a0d64e1a
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2022-11-17 12:06:12 +0100

    Add top Job Description Container

--- src/modules/job/components/JobDescription.vue
+++ src/modules/job/components/JobDescription.vue
@@ -1,22 +1,33 @@
 <template>
   <div :style="mainContainerInlineStyles" class="main-container">
-    <div>
+    <div class="top-container">
       <div>
         <JobCardMetaData
           :contract="currentJobDetails && currentJobDetails.contract"
           :createdAt="currentJobDetails && currentJobDetails.createdAt"
         />
+        <n-h1 class="position">{{
+          currentJobDetails && currentJobDetails.position
+        }}</n-h1>
+        <n-p class="location">{{
+          currentJobDetails && currentJobDetails.location
+        }}</n-p>
       </div>
+      <n-button type="primary">Apply Now</n-button>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import { NP, NH1, NButton } from "naive-ui";
 import JobCardMetaData from "./JobCardMetaData.vue";
 
 export default {
   components: {
+    NP,
+    NH1,
+    NButton,
     JobCardMetaData,
   },
   computed: {
@@ -45,6 +56,20 @@ export default {
 
   @media only screen and (min-width: 768px) {
     margin-top: 132px;
+  }
+
+  .top-container {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    .position {
+      font-weight: 600;
+    }
+    .location {
+      color: #5964e0;
+      font-weight: 600;
+    }
   }
 }
 </style>
